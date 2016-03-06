babb6ced1640ac380caf140e7b01bc0bdab9f0ea
daren-thomas <thomas@arch.ethz.ch>
2016-03-06 16:13:40 +0100

    documentation on data cleaning added

--- clean_data.R
+++ clean_data.R
@@ -1,9 +1,15 @@
 # read in the data, clean it and save only the days we need to disk
-
+# data was obtained from here: https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip,
+# saved to downloads folder and extracted there, resulting in the following file:
 houshold_power_consumption_txt <- '/Users/darthoma/Downloads/household_power_consumption.txt'
+
+# read in the dataset
 df <- read.csv(houshold_power_consumption_txt, sep=';')
+
+# update the columns for Date and Time
 df$Date = as.Date(df$Date, '%d/%m/%Y')
 df$Time <- strptime(df$Time, '%H:%M:%S')
 
+# write out a new, much smaller data frame for creating the plots from...
 df <- subset(df, Date >= as.Date('2007-02-01') & Date <= as.Date('2007-02-02'))
 write.csv(df, 'hpc_cleaned.txt')
